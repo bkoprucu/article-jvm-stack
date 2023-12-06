4ac591c871ed0ef2e7758605d3736a339e83e0fd
Berk Koprucu <bkoprucu@gmail.com>
2023-12-06 18:57:55 +0100

    Add NMT reports, link to the article

--- src/main/java/com/berksoftware/article/jvmstack/MemoryFiller.java
+++ src/main/java/com/berksoftware/article/jvmstack/MemoryFiller.java
@@ -12,6 +12,8 @@ import java.util.stream.IntStream;
 
 /**
  * Allocates stack and heap memory by given parameters for observing JVM behavior under different configurations
+ *
+ * Related to <a href="https://berksoftware.com/2023/12/JVM-Stack-Memory">this blog post.</a>
  */
 public class MemoryFiller {
 
@@ -25,12 +27,13 @@ public class MemoryFiller {
      * @param frameCount    How many stack frames to use per thread
      */
     public void spawnThreads(int threadCount, int heapPerThread, int frameCount) {
-        System.out.println("\nSpawning " + threadCount + " threads, each consuming ~" + heapPerThread + " KB heap, "
+        System.out.println("\nSpawning " + threadCount + " threads, each consuming ~"
+                                   + heapPerThread + " KB heap, "
                                    + frameCount + " stack frames\n");
-
         IntStream.rangeClosed(1, threadCount).forEachOrdered(threadNr -> new Thread(() -> {
             // Allocate heap memory
-             byte[] heapArr = new byte[heapPerThread * 1024];
+            byte[] heapArr = new byte[heapPerThread * 1024];
+
             try {
                 // Allocate stack memory
                 fillStack(frameCount);
@@ -39,24 +42,19 @@ public class MemoryFiller {
                 System.err.println(e);
                 System.exit(1);
             }
-             System.out.print("\r" + threadNr + " threads created");
-             // Block thread for measuring
-             try {
-                 countDownLatch.await();
-             } catch (InterruptedException e) {
-                 throw new RuntimeException(e);
-             }
+            System.out.print("\r" + threadNr + " threads created");
+            // Block thread for measuring
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }).start());
 
         System.out.println("\nPID: " + getCurrentPid());
     }
 
 
-    /**
-     * Fills stack by recursively calling itself
-     *
-     * @param frameCount How many frames to add to stack memory
-     */
     private void fillStack(int frameCount) {
         // Unused primitives defined to consume stack on each frame,
         // since "frames may be heap allocated" : https://docs.oracle.com/javase/specs/jvms/se21/html/jvms-2.html#jvms-2.5.2
@@ -85,12 +83,13 @@ public class MemoryFiller {
     }
 
     public static void main(String[] args) {
-        if(args.length != 3) {
+        if (args.length != 3) {
             System.err.println("Usage: memoryfiller thread_count heap_per_thread stack_frame_per_thread");
             System.exit(1);
         }
-        new MemoryFiller().spawnThreads(Integer.parseInt(args[0]),  // Thread count
-                                        Integer.parseInt(args[1]),  // Heap per thread
-                                        Integer.parseInt(args[2])); // Stack frames per thread
+        new MemoryFiller().spawnThreads(
+                Integer.parseInt(args[0]),  // Thread count
+                Integer.parseInt(args[1]),  // Heap per thread
+                Integer.parseInt(args[2])); // Stack frames per thread
     }
 }
